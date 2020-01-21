2ac9ef6d822f4dd2f1669033f9a6f56c1f59b0cc
thiago <thiago.alessandro.farias@gmail.com>
2020-01-20 23:51:51 -0300

    Arquitetura de microserviço

---
+++ src/main/java/br/com/intelector/microservice/corebrokerkafka/CoreBrokerKafkaApplication.java
@@ -0,0 +1,13 @@
+package br.com.intelector.microservice.corebrokerkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBrokerKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreBrokerKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/intelector/microservice/corebrokerkafka/CoreBrokerKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.intelector.microservice.corebrokerkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBrokerKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

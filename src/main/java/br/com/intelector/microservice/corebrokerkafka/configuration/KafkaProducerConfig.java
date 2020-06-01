3ac62101f63d2dfed8eb0631deca678790e27d55
t.alessandro <thiago.alessandro.farias@gmail.com>
2020-05-31 21:01:36 -0300

    update

--- src/main/java/br/com/intelector/microservice/corebrokerkafka/configuration/KafkaProducerConfig.java
+++ src/main/java/br/com/intelector/microservice/corebrokerkafka/configuration/KafkaProducerConfig.java
@@ -1,25 +1,15 @@
 package br.com.intelector.microservice.corebrokerkafka.configuration;
 
 import br.com.intelector.microservice.coremicroservice.configuration.ApplicationProperties;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
+//@Configuration
 public class KafkaProducerConfig {
 
     @Autowired
     private ApplicationProperties applicationProperties;
 
-    @Bean
+    /*@Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
@@ -30,16 +20,16 @@ public class KafkaProducerConfig {
                 StringSerializer.class);
 
         return props;
-    }
+    }*/
 
-    @Bean
+    /*@Bean
     public ProducerFactory<String, String> producerFactory() {
         return new DefaultKafkaProducerFactory(producerConfigs());
-    }
+    }*/
 
-    @Bean
+    /*@Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate(producerFactory());
-    }
+    }*/
 
 }
